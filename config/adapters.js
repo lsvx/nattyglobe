926f9ae554f91ec72ab400616dcc02cfe60142a1
Lucas Serven <lserven@gmail.com>
2014-06-13 14:09:24 -0400

    Fix spacing

--- config/adapters.js
+++ config/adapters.js
@@ -14,31 +14,31 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'redis',
+    // If you leave the adapter config unspecified 
+    // in a model definition, 'default' will be used.
+    'default': 'redis',
 
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data is preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+    // Persistent adapter for DEVELOPMENT ONLY
+    // (data is preserved when the server shuts down)
+    disk: {
+        module: 'sails-disk'
+    },
 
-  redis: {
-    module: 'sails-redis',
-    port: 6379,
-    host: 'localhost',
-    password: null,
-    options: {
+    redis: {
+        module: 'sails-redis',
+        port: 6379,
+        host: 'localhost',
+        password: null,
+        options: {
 
-        // low-level configuration
-        // (redis driver options)
-        parser: 'hiredis',
-        return_buffers: false,
-        detect_buffers: false,
-        socket_nodelay: true,
-        no_ready_check: false,
-        enable_offline_queue: true
+            // low-level configuration
+            // (redis driver options)
+            parser: 'hiredis',
+            return_buffers: false,
+            detect_buffers: false,
+            socket_nodelay: true,
+            no_ready_check: false,
+            enable_offline_queue: true
+        }
     }
-  }
 };
