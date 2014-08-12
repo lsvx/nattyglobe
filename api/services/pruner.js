45d59e1cdd81a14c6f8c17bbdd3ace97985d02b3
Farhan Syed <nearhan@gmail.com>
2014-08-12 10:49:17 -0400

    fixed pruner service

--- api/services/pruner.js
+++ api/services/pruner.js
@@ -9,19 +9,25 @@ function prune() {
 
         locations.forEach(function(location) {
             var filteredTimestamps  = location.timestamps.filter(function(timestamp) {
+                console.log('timestamp', timestamp);
+                console.log('curr', curr);
                 var diff = (curr - timestamp);
-                if (diff < 300000)  {
+                if (diff < 20000)  {
                     return true;
                 }
                 return false;
             });
 
+            console.log('filteredTimestamps', filteredTimestamps);
             if (filteredTimestamps.length === 0 ) {
 
-                location.destroy(function(err) { console.log(err); });
+                location.destroy(function(err) {
+                    if (err) console.log(err); });
+                    console.log('destroyed record amgio');
 
             } else {
 
+                console.log( 'differeence of array', _.difference(location.timestamps, filteredTimestamps));
                 if( _.difference(location.timestamps, filteredTimestamps).length) {
 
                     Location.update({id: location.id}, {timestamps: filteredTimestamps},
@@ -40,7 +46,7 @@ function prune() {
 // Run this function every 5 minutes
 
 module.exports = (function() {
-    setInterval(prune, 300000);
+    setInterval(prune, 1000);
 })();
 
 
