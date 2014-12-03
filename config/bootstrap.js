9656aa4b107c1d7c3ea097026023b9df1d7884cd
Matias M. Montenegro <matias.montenegro@globant.com>
2014-12-03 09:36:44 -0300

    Fixes in config/bootstrap.js

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -2,16 +2,6 @@ var amqp = require('amqp');
 var messenger = require('../api/services/messenger');
 var pruner = require('../api/services/pruner');
 
-function randIp() {
-
-    var  ipaddress_string = Math.round(Math.random()*255)
-       + '.' + Math.round(Math.random()*255)
-       + '.' + Math.round(Math.random()*255)
-       + '.' + Math.round(Math.random()*255);
-
-    return ipaddress_string;
-}
-
 module.exports.bootstrap = function (cb) {
     var connection = amqp.createConnection({
         host: 'localhost',
@@ -27,8 +17,8 @@ module.exports.bootstrap = function (cb) {
             durable: true
         }, function(queue) {
             queue.subscribe({ack: true}, function(msg) {
-                var msg = msg.data.toString();
-                var j = JSON.parse(msg);
+                var msg = msg.data.toString(),
+                    j = JSON.parse(msg);
                 messenger.newLogin(j[0]);
                 queue.shift(); // basic_ack equivalent
             });
